432cdcb9d1e2ab153c4500643c38c8c47733e3bf
Alessandro Lorenzetti <alessandro.lorenzetti68@gmail.com>
2019-04-01 09:55:17 +0200

    new

---
+++ src/main/java/it/sistnif/demoschematron/DemoSchematronApplication.java
@@ -0,0 +1,13 @@
+package it.sistnif.demoschematron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSchematronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSchematronApplication.class, args);
+	}
+
+}
